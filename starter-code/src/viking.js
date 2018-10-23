2e649f50f5ae972b47795c12abf7447391df1bd3
annearbor <betting.anne@gmail.com>
2018-10-23 22:03:20 +0200

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,59 @@
 // Soldier
-function Soldier() {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+  }
+}
 
-// Viking
-function Viking() {}
+// // Viking
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+    if (this.health > damage) {
+      return this.name + " has received " + damage + " points of damage";
+    } else if (this.health <= damage) {
+      return this.name + " has died in act of combat";
+    }
+  }
+  battleCry() {
+    return "Odin Owns You All!";
+  }
+  attack() {
+    return this.strength;
+  }
+}
 
-// Saxon
-function Saxon() {}
+// // Saxon
+class Saxon extends Soldier {
+  constructor(health, strength) {
+    super(health, strength);
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+    if (this.health > 0) {
+      return "A Saxon has received " + damage + " points of damage";
+    } else if (this.health <= 0) {
+      return "A Saxon has died in combat";
+    }
+  }
+}
+// class War {
+//     constructor() {
+//         vikingArmy =
 
-// War
-function War() {}
+//     }
+// }
